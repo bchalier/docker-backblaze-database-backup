9e66f648256057a35b79235f285159028e32190e
Bastien Chalier <bastien@chalier.io>
2020-11-07 16:41:30 +0100

    folderize remote path

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 # /bin/bash
 
 filename=dump.sql
-remoteFilename="${DB_DATABASE}/$(date '+%Y-%m-%d_%Hh%M').sql.gz"
+remoteFilename="${DB_DATABASE}_$(date '+%Y-%m-%d_%Hh%M').sql.gz"
+remoteFilePath="${DB_DATABASE}/$(date '+%Y')/$(date '+%B')/$(date '+%d')/${remoteFilename}"
 
 echo "exporting $DB_DATABASE database"
 mysqldump -h $DB_HOST -u $DB_USERNAME -p"$DB_PASSWORD" $DB_DATABASE > $filename -v
@@ -13,4 +14,4 @@ echo "authenticating with backblaze"
 b2 authorize-account $B2_KEY_ID $B2_KEY
 
 echo "uploading $remoteFilename to backblaze B2"
-b2 upload-file $B2_BUCKET $filename.gz $remoteFilename
+b2 upload-file $B2_BUCKET $filename.gz $remoteFilePath
